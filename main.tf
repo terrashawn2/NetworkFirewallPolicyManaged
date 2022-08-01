a034760f3cc165debe8f60a4e1719f5a62d3d205
terrashawn2 <110316525+terrashawn2@users.noreply.github.com>
2022-08-01 18:05:40 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_networkfirewall_firewall_policy" "testpolicy" {
+
+  name = "testpolicy"
+  tags = {
+    "Name" = "testfwpolicy"
+  }
+
+
+  firewall_policy {
+    stateful_default_actions = []
+    stateless_default_actions = [
+      "aws:forward_to_sfe",
+    ]
+    stateless_fragment_default_actions = [
+      "aws:forward_to_sfe",
+    ]
+
+    stateful_engine_options {
+      rule_order = "DEFAULT_ACTION_ORDER"
+    }
+
+    stateful_rule_group_reference {
+      priority     = 1
+      resource_arn = "arn:aws:network-firewall:us-east-1:aws-managed:stateful-rulegroup/AbusedLegitMalwareDomainsActionOrder"
+    }
+    stateful_rule_group_reference {
+      priority     = 2
+      resource_arn = "arn:aws:network-firewall:us-east-1:aws-managed:stateful-rulegroup/MalwareDomainsActionOrder"
+    }
+  }
+}
