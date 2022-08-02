176029af7a2c9fa29f39427f49a78eb8ff942e8a
terrashawn2 <110316525+terrashawn2@users.noreply.github.com>
2022-08-01 21:04:07 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,15 @@
-variable "awsregion" {
+variable "fw_region" {
  type = string
  default = "us-east-1"
+}
+
+variable "fw_name" {
+ type = string
+ default = "aws_managed_rules_fw_policy"
+}
+
+variable "fw_env" {
+ type = string
+ default = "DEV"
 }
 
